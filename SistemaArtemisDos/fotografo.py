a13809247e4e535203f3094a9e357077fc17b128
Javier <javi.IES9023@gmail.com>
2026-04-12 19:33:16 -0300

    Creo clase Fotografo

---
+++ SistemaArtemisDos/fotografo.py
@@ -0,0 +1,5 @@
+from astronauta import Astronauta
+
+class Fotografo(Astronauta):
+    def tarea(self):
+        print(f"{self.nombre} saca fotos por todos lados...")
--- SistemaArtemisDos/main.py
+++ SistemaArtemisDos/main.py
@@ -3,6 +3,8 @@ from ingeniero import Ingeniero
 from cientifico import Cientifico
 from nave import Nave
 from mision import Mision
+from fotografo import Fotografo
+
 
 # Crear nave
 nave = Nave("Orion", "Tripulada", 4)
@@ -14,11 +16,13 @@ mision = Mision("Artemis II", "Luna", 10, nave)
 astronauta1 = Piloto("Juan", 40)
 astronauta2 = Ingeniero("Ana", 38)
 astronauta3 = Cientifico("Luis", 45)
+astronauta4 = Fotografo("Marcelo",20)
 
 # Agregar a la misión
 mision.agregar_astronauta(astronauta1)
 mision.agregar_astronauta(astronauta2)
 mision.agregar_astronauta(astronauta3)
+mision.agregar_astronauta(astronauta4)
 
 # Iniciar misión
 mision.iniciar_mision()
